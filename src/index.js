cc050c1053f3c0f04a45f1e4d8a1d3318654bc95
Autist69420 <56086819+Autist69420@users.noreply.github.com>
2021-05-13 00:28:42 +0200

    Actually make it work
    i sure do love late night coding

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 let args = process.argv.slice(2);
 
-let url = "https://api.r34.app/booru/gelbooru/posts?baseEndpoint=" + args[0]  + "&limit=" + args[1] + "&pid=0&tags=" + process.argv.slice(4).join("+") + "&tagsEndpoint=%2Fautocomplete.php&defaultQueryIdentifiersTagsTag=q&defaultQueryIdentifiersTagsTagEnding=";
+let url = "https://api.r34.app/booru/gelbooru/posts?baseEndpoint=" + args[0]  + "&limit=" + args[1] + "&tags=" + process.argv.slice(4).join("+");
 
-const axios = require("axios").default;
+const fetch = require("node-fetch");
 const request = require("request");
 const path = require("path");
 const fs = require("fs");
@@ -14,14 +14,17 @@ const download = async(uri, filename, callback) => {
 };
 
 (async() => {
-    await axios.get(url).then(r => {
-        r.data.forEach(img => {
-            let split = img.high_res_file.url.split(".")
-            let ext = split[3];
-
-            download(img.high_res_file.url, path.join(__dirname, "/../out/" + img.id.toString() + `.${ext}`), function(){
-                console.log('done');
-            });
-        });
+    await fetch(url).then(r => {
+        r.json().then(data => {
+            data.forEach(img => {
+                let split = img.high_res_file.url.split(".")
+                let ext = split[3];
+                console.log(`starting download of file ${img.id.toString()}.${ext}`)
+                
+                download(img.high_res_file.url, path.join(__dirname, "/../out/" + img.id.toString() + `.${ext}`), function(){
+                    console.log(`Downloaded file ${img.id.toString()}.${ext}`);
+                });
+            })
+        })
     });
 })()
